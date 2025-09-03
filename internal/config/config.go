ce63f565f600748943f0b07247ce91bb601cebc0
Guillem <guillemsuetta1@gmail.com>
2025-09-02 21:00:43 -0300

    refactor(config): Log the configuration on startup (excluding secrets)

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -14,14 +15,29 @@ type Config struct {
 	SQSQueueURL  string
 }
 
+func (c *Config) String() string {
+	// A simple way to hide a password in a URL.
+	safePostgresURL := "postgres://user:***@postgres:5432/credit_ledger?sslmode=disable"
+	return fmt.Sprintf(
+		"Config{PostgresURL: %s, DynamoDBURL: %s, KafkaBrokers: %v, AWSRegion: %s, SQSQueueURL: %s}",
+		safePostgresURL,
+		c.DynamoDBURL,
+		c.KafkaBrokers,
+		c.AWSRegion,
+		c.SQSQueueURL,
+	)
+}
+
 func Load() *Config {
-	return &Config{
-		PostgresURL:  getEnv("POSTGRES_URL", "postgres://user:password@postgres:5432/credit_ledger?sslmode=disable"),
+	cfg := &Config{
+		PostgresURL:  getEnv("POSTG-RES_URL", "postgres://user:password@postgres:5432/credit_ledger?sslmode=disable"),
 		DynamoDBURL:  getEnv("DYNAMODB_URL", "http://dynamodb:8000"),
 		KafkaBrokers: strings.Split(getEnv("KAFKA_BROKERS", "kafka:9092"), ","),
 		AWSRegion:    getEnv("AWS_REGION", "us-east-1"),
 		SQSQueueURL:  getEnv("SQS_QUEUE_URL", "http://localhost:4566/000000000000/my-queue"),
 	}
+	log.Printf("Loaded configuration: %s", cfg.String())
+	return cfg
 }
 
 func getEnv(key, fallback string) string {
